8eaa9d10938f055d6a42c8d27ad45ac2068a57d6
dof-bull <donalofarrell1@gmail.com>
2020-07-20 11:22:40 +0000

    MongoDB connection made

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'rimworld_mod_locker'
+app.config["MONGO_URI"] = 'mongodb+srv://dbUser:Iwant3mods@cluster0.aadrh.mongodb.net/rimworld_mod_locker?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_mods')
+def get_mods():
+    return render_template("mods.html", mods=mongo.db.mods.find())
 
 
 if __name__ == '__main__':
