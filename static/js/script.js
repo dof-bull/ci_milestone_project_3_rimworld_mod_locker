63480ba7eff7909e956495a04a99ad5d01878eda
dof-bull <donalofarrell1@gmail.com>
2020-07-22 18:13:33 +0000

    Added bug fix to selector needing to be clicked twice

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,12 @@ $(document).ready(function () {
     $('select').material_select();
     $(".button-collapse").sideNav();
 });
+
+//----------Stops the selector needing to be clicked twice. Bug Fix by Simon Castagna----------
+
+$(document).on('mousedown', '#picker-input', function (event) {
+    event.preventDefault();
+});
+$(document).on('change select', (e) => {
+    $('select').material_select();
+});
